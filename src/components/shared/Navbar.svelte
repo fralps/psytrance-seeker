91e45dd792e0e79394c048ec75bfb98502b9ce57
François Loupias <48285890+fralps@users.noreply.github.com>
2022-01-31 15:37:25 +0100

    feature: add tailwind + i18n (#1)

---
+++ src/components/shared/Navbar.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { locale, locales } from '../../services/i18n/i18n';
+</script>
+
+<section>
+  <select class="border m-4 cursor-pointer" bind:value={$locale}>
+    { #each locales as locale }
+      <option value={locale}>{ locale }</option>
+    { /each }
+  </select>
+</section>
