a5bcae15bc5cd8a588ca232ac23192db22ff4f1f
fralps <francois@kinoba.fr>
2022-01-31 15:58:42 +0100

    feature: add eslint config + fix warnings

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,42 @@
 <script>
   import { onMount } from 'svelte';
-  import axios from 'axios';
-  import { toJson } from './helpers/youtubeHelper.js';
+  // import axios from 'axios';
+  // import { toJson } from './helpers/youtubeHelper.js';
 
   // Main styles
-  import logo from './assets/svelte.png';
+  // import logo from './assets/svelte.png';
   import './components/shared/TailwindCSS.svelte';
 
   // Components
   import Navbar from './components/shared/Navbar.svelte';
   import Quote from './components/Quote.svelte';
 
-  let results;
+  // let results;
 
   onMount(async () => {
     // fetchYoutubeList();
   });
 
-  const fetchYoutubeList = () => {
-    const searchQuery = 'psytrance';
-    const maxResults = 10;
-    const apiKey = import.meta.env.VITE_APP_YOUTUBE_API_KEY;
-    const apiBaseUrl = import.meta.env.VITE_APP_API_BASE_URL;
+  // const fetchYoutubeList = () => {
+  //   const searchQuery = 'psytrance';
+  //   const maxResults = 10;
+  //   const apiKey = import.meta.env.VITE_APP_YOUTUBE_API_KEY;
+  //   const apiBaseUrl = import.meta.env.VITE_APP_API_BASE_URL;
 
-    axios.get(`${apiBaseUrl}`, {
-      params: {
-        part: 'snippet',
-        maxResults: maxResults,
-        q: searchQuery,
-        type: 'video',
-        key: apiKey
-      }
-    })
-      .then(response => {
-        console.log(response.data);
-        results = toJson(response.data.items);
-      });
-  };
+  //   axios.get(`${apiBaseUrl}`, {
+  //     params: {
+  //       part: 'snippet',
+  //       maxResults: maxResults,
+  //       q: searchQuery,
+  //       type: 'video',
+  //       key: apiKey
+  //     }
+  //   })
+  //     .then(response => {
+  //       console.log(response.data);
+  //       results = toJson(response.data.items);
+  //     });
+  // };
 </script>
 
 <section class="text-gray-600 body-font">
