4160c07d607d99f7d630027bbaf628bbcdb1b160
François Loupias <48285890+fralps@users.noreply.github.com>
2022-02-07 14:52:21 +0100

    feat(font): add Bluunext font + some refacto (#7)

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
+  // Translations service
   import { t } from './services/i18n/i18n';
 
   // Main styles
-  import logo from './assets/boom.png';
   import './components/shared/TailwindCSS.svelte';
+  import './assets/styles/app.css';
 
   // Components
   import Navbar from './components/shared/Navbar.svelte';
@@ -13,7 +14,6 @@
 <section class="text-gray-600 body-font">
   <Navbar />
   <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={logo}>
     <div class="text-center w-full">
       <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
         { $t('page_title') }
