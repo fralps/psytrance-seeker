a2198cdf7cb69fa1529e2250a30fda61932fa8b1
François Loupias <48285890+fralps@users.noreply.github.com>
2022-02-07 14:15:32 +0100

    feat(wip): add card component + props and some styles (#6)

--- src/components/Quote.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { t } from '../services/i18n/i18n';
-</script>
-
-<section>
-  <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-    { $t('page_title') }
-  </h1>
-</section>
---
+++ src/components/youtube/Youtube.svelte
@@ -0,0 +1,44 @@
+<script>
+  import axios from 'axios';
+  import Card from './Card.svelte';
+
+  let searchTerm;
+  let videos = [];
+
+ const fetchYoutubeList = async () => {
+    const searchQuery = 'psytrance';
+    const maxResults = 10;
+    const apiKey = import.meta.env.VITE_APP_YOUTUBE_API_KEY;
+    const apiBaseUrl = import.meta.env.VITE_APP_API_BASE_URL;
+
+    searchTerm = searchQuery;
+
+    await axios.get(`${apiBaseUrl}`, {
+      params: {
+        part: 'snippet',
+        maxResults: maxResults,
+        q: searchQuery,
+        type: 'video',
+        key: apiKey
+      }
+    })
+      .then(response => {
+        videos = response.data.items;
+      });
+  };
+</script>
+
+<section class="container">
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+    on:click|once={fetchYoutubeList}
+  >
+    Bring me joy
+  </button>
+
+  <div class="text-center grid grid-cols-3 gap-4">
+    {#each videos as video }
+      <Card video={video} searchTerm={searchTerm} />
+    {/each}
+  </div>
+</section>
